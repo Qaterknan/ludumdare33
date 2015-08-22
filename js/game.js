762220d00d2038e2d473a42b47014fc83e96cc3e
Jirka Balhar <balhar.j@gmail.com>
2015-08-22 11:02:44 +0200

    Ovládání rychlostí

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ PhaserGame.prototype = {
     preload: function () {
         game.load.image('tree','images/tree.png');
         game.load.image('blood','images/blood.png');
+        game.load.spritesheet('buttons','images/buttons.png', 16, 16);
         game.load.spritesheet('soldier', 'images/soldier.png', 8, 8);
     },
     create: function () {
@@ -16,6 +17,29 @@ PhaserGame.prototype = {
         game.world.setBounds(0, 0, 8000, 480);
         game.stage.backgroundColor = '#dddddd';
         game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        // gui
+        var pauseButton = game.add.button(400-60, 400, "buttons", function(){
+            this.march.setSpeed(0);
+        }, this, 0, 0);
+        pauseButton.scale.set(2);
+        pauseButton.smoothed = false;
+        var speed1Button = game.add.button(400-20, 400, "buttons", function(){
+            this.march.setSpeed(1);
+        }, this, 1, 1);
+        speed1Button.scale.set(2);
+        speed1Button.smoothed = false;
+        var speed2Button = game.add.button(400+20, 400, "buttons", function(){
+            this.march.setSpeed(2);
+        }, this, 2, 2);
+        speed2Button.scale.set(2);
+        speed2Button.smoothed = false;
+        var speed3Button = game.add.button(400+60, 400, "buttons", function(){
+            this.march.setSpeed(3);
+        }, this, 3, 3);
+        speed3Button.scale.set(2);
+        speed3Button.smoothed = false;
+
 
         // pokusy
         this.march = new March(this.game);
