d15504043d183b6b0451c4bbbe35901a168c989e
Jirka Balhar <balhar.j@gmail.com>
2015-08-24 11:22:32 +0200

    Server je creep

--- stats.php
+++ stats.php
@@ -3,6 +3,6 @@
 // $.ajax("stats.php", {data:{stats: JSON.stringify(progress)}, method:"POST"})
 if (isset($_POST["stats"])) {
 	$ip = $_SERVER['REMOTE_ADDR'];
-	file_put_contents("stats.json", $_POST["stats"], FILE_APPEND);
+	file_put_contents("stats.json", $ip . ": " . $_POST["stats"] . "\n", FILE_APPEND);
 }
 ?>
